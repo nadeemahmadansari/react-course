109c6104cab9756e1c3d204c4a611efcdf5d529b
Nadeem Ahmad <nadeem2136@gmail.com>
2023-04-05 11:31:14 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h1", {}, "This is React Course🚀"),
     React.createElement("h2", {}, "I'm h2 tag"),
   ]),
   React.createElement("div", { id: "child" }, [
@@ -12,7 +15,5 @@ const parent = React.createElement("div", { id: "parent" }, [
 console.log("Parent: ", parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const root2 = ReactDOM.createRoot(document.getElementById("root2"));
 
 root.render(parent);
-root2.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # React Course 🚀
+
+# Parcel
+
+- Dev build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification(Production Build)
+- Bundling
+- Compress
+- Consistent Hashng
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- Host on HTTPs
+- Tree Shaking - remove unused code
+- Different dev and production bundles
